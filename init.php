f334cd0d630e0027c39d750b97a523ca19266df5
GabHaf <gabhaf-6@student.ltu.se>
2021-11-11 10:45:51 +0000

    adding init.php

---
+++ init.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "utbweb.its.ltu.se";
+$username = "19970225";
+$password = "19970225";
+$databas = "db19970225";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $databas);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+?> 
+<p>We got it working</p>
+<p> mvh Bella </p>
