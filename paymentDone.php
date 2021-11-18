9626f438be7e37ff53f82fb540a8fc51257c209f
gabhaf <gabhaf-6@student.ltu.se>
2021-11-18 17:32:27 +0100

    initial pamentdone +

---
+++ paymentDone.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Thank you - bestshop</title>
+</head>
+
+<!--
+<?php include 'init.php';?>
+-->
+
+<body>
+	<p>Thank you for choosing best shop</p>
+
+
+
+</body>
--- paymentPage.php
+++ paymentPage.php
@@ -8,7 +8,28 @@
 
 <body>
 	<p>Test</p>
+<?php
+/*
+	<form action="paymentDone.php" method="get">
+	Adress: <input type="text" name="adress"><br>
+	E-mail: <input type="text" name="email"><br>
+	<input type="submit">
+	</form>
+*/
+	$adressErr = $emailErr = "";
 
-
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		if (empty($_POST["adress"])) {
+			$adressErr = "Adress is required";
+		} else {
+			$name = test_input($_POST["adress"]);
+		}
+		
+		if (empty($_POST["email"])) {
+			$emailErr = "Email is required";
+		} else {
+			$email = test_input($_POST["email"]);
+		}
+?>
 
 </body>
