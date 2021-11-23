0f96de8da4dfa780b448bc1c67a81d58e7191c2a
GabHaf <gabhaf-6@student.ltu.se>
2021-11-23 07:28:54 +0000

    paymentRelated

--- paymentDone.php
+++ paymentDone.php
@@ -4,9 +4,101 @@
 	<title>Thank you - bestshop</title>
 </head>
 
-<!--
-<?php include 'init.php';?>
--->
+
+<?php
+	include "init.php";
+	$conn = connect();
+	$userId = "2";
+	$orderId = "1";
+
+	$adress = $_POST["adress"];
+	$email = $_POST["email"];
+
+	echo $adress;
+
+//Create an order table to store the order
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+* Note:
+*	OrderId and userId connect to an itemList for that specific order
+*	So to find the times of the order, use those and serch through itemList
+*
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+//	$sqlForTable = "SELECT * FROM products";
+//	$sqlForColumns = "SHOW COLUMNS FROM products";
+
+	// Creates the order table
+	$sqlOrder = "INSERT INTO `orders` (`orderId`, `userId`, `adress`, `status`, `message`) VALUES ('$orderId', '$userId', '$adress', '1', '')";
+
+	$result = mysqli_query($conn, $sqlOrder);
+
+
+
+//Alter the database to have the correct amount of products
+
+//	$sqlItems = "SELECT amount, productId FROM itemList WHERE orderId == $orderId AND userId == $userId";
+	// Gives productId and amount of the users "shopping cart"
+	$sqlItems = "SELECT `productId`, `amount` FROM `itemList` WHERE `orderId`=$orderId AND `userId`=$userId";
+	$itemQuery = mysqli_query($conn, $sqlItems);
+
+if (mysqli_num_rows($itemQuery) > 0) {
+
+
+while($itemArray = mysqli_fetch_array($itemQuery)) {
+//	echo $itemArray[amount];
+//	echo $itemArray[productId];
+
+
+	// Gives the current amount of specifide product
+	$sqlProd  = "SELECT `amount` FROM `products` WHERE `productId`= $itemArray[productId] ";
+	$amountProd = mysqli_query($conn, $sqlProd);
+	$amountProd = mysqli_fetch_array($amountProd);
+//	echo $amountProd[amount];
+
+	// Sets the new amount
+	$newAmount = $amountProd[amount]-$itemArray[amount];
+	// Error check to make sure it's not negative
+	// Not fully sure how to handle that kind of error
+	// Should we make sure that no amount is negative and cancel?
+	if($newAmount < 0){
+		echo "Error: Missing product, can't buy that many ";
+	}
+
+//	$newAmount = "47";
+//	echo $newAmount;
+
+	$sql = "UPDATE `products` SET `amount` = $newAmount WHERE `products`.`productId` = $itemArray[productId]";
+	$endResult = mysqli_query($conn, $sql);
+}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                if (empty($_POST["adress"])) {
+                        $adressErr = "Adress is required";
+			echo $adressErr;
+                } else {
+                        $name = test_input($_POST["adress"]);
+                }
+
+                if (empty($_POST["email"])) {
+                        $emailErr = "Email is required";
+			echo $emailErr;
+                } else {
+                        $email = test_input($_POST["email"]);
+                }
+}*/
+?>
 
 <body>
 	<p>Thank you for choosing best shop</p>
@@ -14,3 +106,4 @@
 
 
 </body>
+
--- paymentPage.php
+++ paymentPage.php
@@ -1,35 +1,112 @@
+<?php
+// Start the session
+session_start();
+?>
+<script>
+//check if user is not member
+var userType = '<?=$_SESSION["status"]?>';
+
+switch(userType) {
+    case "admin":
+	window.location.href = "/admin_start.php";
+        break;
+
+    case "distributer":
+	window.location.href = "/distributerStart.php";
+        break;
+
+    case "member":
+        break;
+
+    default:
+//       window.location.replace("http://130.240.200.56/paymentPage.php");
+
+}
+
+</script>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<title>Payment Page - bestshop</title>
 </head>
 
-<?php include 'init.php';?>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round|Open+Sans">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<style type="text/css">
+.bs-example{
+margin: 20px;
+}
+</style>
+<script type="text/javascript">
+$(document).ready(function(){
+$('[data-toggle="tooltip"]').tooltip();
+});
+</script>
 
+<?php 
+	include "init.php";
+	$conn = connect();
+?>
 <body>
-	<p>Test</p>
+
+<div class="bs-example">
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<div class="page-header clearfix">
+<h2 class="pull-left">Payment Page</h2>
+</div>
+
 <?php
-/*
-	<form action="paymentDone.php" method="get">
+// Welcome the user
+$userId = "2"; //member=2, admin=1
+$orderId = "1";
+
+$userName = "SELECT `name` FROM `users` WHERE `userId`=$userId";
+$userName = mysqli_query($conn,$userName);
+$name = mysqli_fetch_array($userName);
+?>
+	<h4> Welcome <?php echo $name[name];?> </h4>
+
+<?php
+// Tell of what products are in the shoping cart
+$sqlItems = "SELECT `productId`, `amount` FROM `itemList` WHERE `orderId`=$orderId AND `userId`=$userId";
+$items = mysqli_query($conn,$sqlItems); //Items in cart
+$prodInfo = mysqli_query($conn,"SELECT `name`,`image` FROM products");
+
+?>
+<?php
+if (mysqli_num_rows($prodInfo) > 0 && mysqli_num_rows($items) > 0 ) {
+?>
+<table class='table table-bordered table-striped'>
+<tr> <td>Product</td> <td>Amount</td> <td></td> </tr>
+
+<?php
+// Running through and printing the users shopping cart
+while($row = mysqli_fetch_array($prodInfo)) {
+	$amount = mysqli_fetch_array($items);
+?>	<tr>
+	<td><?php echo $row["name"]; ?></td> <td><?php echo $amount["amount"]; ?></td>
+	<td><img src="<?php echo $row['image']; ?>" style="width:50px;height:50px;"  >
+	</tr>
+<?php
+}}
+?>
+</table>
+
+	<form action="/paymentDone.php" method="post">
 	Adress: <input type="text" name="adress"><br>
 	E-mail: <input type="text" name="email"><br>
-	<input type="submit">
+	Buy: <input type="submit">
 	</form>
-*/
-	$adressErr = $emailErr = "";
 
-	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		if (empty($_POST["adress"])) {
-			$adressErr = "Adress is required";
-		} else {
-			$name = test_input($_POST["adress"]);
-		}
-		
-		if (empty($_POST["email"])) {
-			$emailErr = "Email is required";
-		} else {
-			$email = test_input($_POST["email"]);
-		}
+<?php
+// We are only looking for input on paymentpage
+disConnect($conn);
 ?>
-
 </body>
