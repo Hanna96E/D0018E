727147799438fb1a4b0cdd9618648d4ede3d9f8b
gabhaf <gabhaf-6@student.ltu.se>
2021-11-23 10:18:34 +0100

    följer hannas order part 2

--- member_start.php
+++ member_start.php
@@ -62,6 +62,7 @@ $('[data-toggle="tooltip"]').tooltip();
 <!---<td><?php// echo $row["id"]; ?></td>--->
 <td><a href="/member_start.php"><button> Home </button></a></td>
 <td><a href="/productsForMember.php"><button> View products </button></a></td>
+<td><a href="/memberCart.php"><button> View cart </button></a></td>
 <td><a href="/logout.php"><button> Log out </button></a></td>
 </tr>
 </table>
