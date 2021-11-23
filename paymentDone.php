659e6847942442584db5daa4b0c291fe6130dd20
gabhaf <gabhaf-6@student.ltu.se>
2021-11-23 09:59:25 +0100

    added menubar for members

--- paymentDone.php
+++ paymentDone.php
@@ -5,6 +5,21 @@
 </head>
 
 
+
+<!--MEMBER MENUE BAR-->
+<table>
+<tr>
+<!---<td><?php// echo $row["id"]; ?></td>--->
+<td><a href="/member_start.php"><button> Home </button></a></td>
+<td><a href="/productsForMember.php"><button> View products </button></a></td>
+<td><a href="/memberCart.php"><button> View cart </button></a></td>
+<td><a href="/logout.php"><button> Log out </button></a></td>
+</tr>
+
+
+
+
+
 <?php
 	include "init.php";
 	$conn = connect();
--- paymentPage.php
+++ paymentPage.php
@@ -48,6 +48,22 @@ $('[data-toggle="tooltip"]').tooltip();
 });
 </script>
 
+
+<!--MEMBER MENUE BAR-->
+<table>
+<tr>
+<!---<td><?php// echo $row["id"]; ?></td>--->
+<td><a href="/member_start.php"><button> Home </button></a></td>
+<td><a href="/productsForMember.php"><button> View products </button></a></td>
+<td><a href="/memberCart.php"><button> View cart </button></a></td>
+<td><a href="/logout.php"><button> Log out </button></a></td>
+</tr>
+
+
+
+
+
+
 <?php 
 	include "init.php";
 	$conn = connect();
