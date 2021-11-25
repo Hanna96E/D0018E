ce4c3b02b40a03ce099b1b4756c65db8d7b6d148
gabhaf <gabhaf-6@student.ltu.se>
2021-11-25 16:18:35 +0100

    101

--- functions101.php
+++ functions101.php
@@ -1,4 +1,4 @@
-x<?php
+<?php
 
 
 // security functions start-------------------------------------------------------------------
@@ -43,11 +43,11 @@ function showOrderStatus($status){
 function addOrRemove1ToCartForMember($conn,$redirectToString,$userId,$orderId,$row,$amountInCart){
 
     // options
-    $textOnAddButton = "+";
-    $textOnRemoveButton = "-";
+    $textOnAddButton = "add";
+    $textOnRemoveButton = "remove";
 
     $nameForNumberBox = "amountInNumberBox"; // do not change
-    $textOnNumberSubmitButton = "change";
+    $textOnNumberSubmitButton = "submit number";
     
     $productIdValue = $row["productId"];
 
@@ -71,7 +71,7 @@ function addOrRemove1ToCartForMember($conn,$redirectToString,$userId,$orderId,$r
       echo $textOnNumberSubmitButton;
       echo "\" value=\"".$textOnNumberSubmitButton."\">";
 
-
+      echo "<br>";
 
     echo "</form>";
 
@@ -178,7 +178,7 @@ function showProductsForMember($conn,$userId,$orderId,$tableClassName){
 
   echo "<tr>";
   showTableHeader(array("name","price","info","image"));
-  echo "<th></th><th></th>";
+  echo "<th></th>";
   echo "</tr>";
   
   $sqlQueryResult = mysqli_query($conn,$sql);
@@ -270,6 +270,9 @@ function showMemberCart($conn,$userId,$orderId,$tableClassName){
     foreach ($columnNameToShowArray as $columnName) {
         echo "<td>";
         showSimple($conn,$columnName,$row);
+        if($columnName = "price"){
+            $totalCost = $totalCost + $row["price"];
+        }  
         echo "</td>";
     }
 
@@ -280,7 +283,10 @@ function showMemberCart($conn,$userId,$orderId,$tableClassName){
     addOrRemove1ToCartForMember($conn,$redirectToString,$userId,$orderId,$row,$amountInCart);
     echo "</td>";
 
+    echo "<td>";
     
+    //$conn
+    //$redirectToString
     $tableName = "itemList";
     $primaryKeyColumnNameArray = array("listId");
     $row = $itemListRowArray[$i];
