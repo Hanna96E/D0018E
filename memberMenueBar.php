5985fa2588db59cf0050104afa2e3a631ddde8be
GabHaf <74302691+GabHaf@users.noreply.github.com>
2021-12-16 18:49:22 +0100

    Delete memberMenueBar.php

--- memberMenueBar.php
+++
@@ -1,7 +0,0 @@
-<!--MEMBER MENUE BAR-->
-<table><tr>
-<td><a href="/member_start.php"><button> Home </button></a></td>
-<td><a href="/productsForMember.php"><button> View products </button></a></td>
-<td><a href="/memberCart.php"><button> View cart </button></a></td>
-<td><a href="/logout.php"><button> Log out </button></a></td>
-</tr></table><br><br>
